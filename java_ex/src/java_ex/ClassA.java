d391aad68d6dc39b00e22fba4545879f738b00fe
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-10 10:00:08 +0900

    UPDATE use Scanner package

--- java_ex/src/java_ex/ClassA.java
+++ java_ex/src/java_ex/ClassA.java
@@ -1,7 +1,7 @@
 package java_ex;
 
 public class ClassA {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ java_ex/src/java_ex/ClassB.java
@@ -0,0 +1,15 @@
+package java_ex;
+
+import java.util.Scanner;
+
+public class ClassB {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String str = scan.next();
+
+		System.out.println(str);
+	}
+}
