435f64d608883fd9b3b7a2ff28e9651c85737e9e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-12 15:15:21 +0900

    CREATE string equals

---
+++ java_ex/src/java_ex/ClassE.java
@@ -0,0 +1,14 @@
+package java_ex;
+
+public class ClassE {
+	public static void main(String[] args) {
+		String[] array = new String[3];
+		array[0] = "JAVA";
+		array[1] = "JAVA";
+		array[2] = new String("JAVA");
+		
+		System.out.println(array[0] == array[1]);
+		System.out.println(array[0] == array[2]);
+		System.out.println(array[1].equals(array[2]));
+	}
+}
