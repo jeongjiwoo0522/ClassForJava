7e281e5ce546d92c3e3f38c01a94e6ec9ed8b60d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-12 15:00:42 +0900

    UPDATE array

---
+++ java_ex/src/java_ex/ClassD.java
@@ -0,0 +1,19 @@
+package java_ex;
+
+public class ClassD {
+	public static void main(String[] args) {
+		int[] arr = new int[10]; 
+		for(int i=0; i<10; i++) {
+			arr[i] = i+1;
+		}
+		System.out.println(add(arr));
+	}
+	
+	public static int add(int[] arr) {
+		int sum = 0;
+		for(int i=0; i<arr.length; i++) {
+			sum += arr[i];
+		}
+		return sum;
+	}
+}
