58a84fc7cd75bb870d504a7f241b7529165d6b07
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-17 09:07:01 +0900

    CREATE AgAdvancedForExample.java

---
+++ java_ex/src/java_ex/AgAdvancedForExample.java
@@ -0,0 +1,14 @@
+package java_ex;
+
+public class AgAdvancedForExample {
+	
+	public static void main(String[] args) {
+		String[] arr = new String[5];
+		arr[0] = "JAVA";
+		arr[1] = "Class";
+		
+		for(String str : arr) {
+			System.out.println(str);
+		}
+	}
+}
