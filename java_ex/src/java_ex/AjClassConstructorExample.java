bb9e0da082e2241fb6f0426178828f6803636f2b
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-19 13:50:09 +0900

    CREATE class constructor example

---
+++ java_ex/src/java_ex/AjClassConstructorExample.java
@@ -0,0 +1,15 @@
+package java_ex;
+
+public class AjClassConstructorExample {
+	
+	String name;
+	int a;
+	
+	AjClassConstructorExample() {
+		// default 생성자 
+	}
+	
+	AjClassConstructorExample(String name) {
+		this.name = name;
+	}
+}
