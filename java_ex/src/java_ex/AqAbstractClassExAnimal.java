4d5920c467d6fe414b2151c12371d24a5b2b8e64
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-14 09:57:51 +0900

    CREATE AqAbstractClassExAnimal.java

---
+++ java_ex/src/java_ex/AqAbstractClassExAnimal.java
@@ -0,0 +1,54 @@
+package java_ex;
+
+abstract class Animal {
+	 
+	public String kind;
+	
+	public void breath() {
+		System.out.println("숨을 쉽니다");
+	}
+	
+	public abstract void sound();
+}
+
+class Cat extends Animal {
+	
+	public Cat() {
+		this.kind = "포유류";
+	}
+	
+	@Override
+	public void sound() {
+		System.out.println("야옹");
+	}
+}
+
+class Dog extends Animal {
+	
+	public Dog() {
+		this.kind = "포유류";
+	}
+	
+	@Override
+	public void sound() {
+		System.out.println("멍멍");
+	}
+}
+
+public class AqAbstractClassExAnimal {
+	
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		Cat cat = new Cat();
+		dog.sound();
+		cat.sound();
+		System.out.println();
+		
+		Animal animal = null;
+		animal = new Dog();
+		animal.sound();
+		
+		animal = new Cat();
+		animal.sound();
+	}
+}
