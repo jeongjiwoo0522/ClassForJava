c3b4452d8c028f5a66d0b38b1d555b5da6e60a95
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-16 15:20:53 +0900

    CREATE RemoteControlExample.java

---
+++ java_ex/src/test_0416/RemoteControlExample.java
@@ -0,0 +1,18 @@
+package test_0416;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		RemoteControl rc = new RemoteControl() {
+			
+			@Override
+			public void turnOn() {}
+			
+			@Override
+			public void turnOff() {}
+			
+			@Override
+			public void setVolume(int volume) {}
+		};
+	}
+}
