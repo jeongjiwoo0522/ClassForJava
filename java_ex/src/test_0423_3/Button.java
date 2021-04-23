c3d6822410ded3034bf575c65bab6e5a087f2f1c
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-23 15:19:52 +0900

    CREATE nestclass example

---
+++ java_ex/src/test_0423_3/Button.java
@@ -0,0 +1,18 @@
+package test_0423_3;
+
+public class Button {
+
+	interface OnClickListener {
+		void onClick();
+	}
+	
+	OnClickListener listener;
+	
+	void setOnClickListener(OnClickListener listener) {
+		this.listener = listener;
+	}
+	
+	void touch() {
+		listener.onClick();
+	}
+}
---
+++ java_ex/src/test_0423_3/MainTest.java
@@ -0,0 +1,14 @@
+package test_0423_3;
+
+public class MainTest {
+	
+	public static void main(String[] args) {
+		Button btn = new Button();
+		
+		btn.setOnClickListener(new CallListener());
+		btn.touch();
+		
+		btn.setOnClickListener(new MessageListener());
+		btn.touch();
+	}
+}
