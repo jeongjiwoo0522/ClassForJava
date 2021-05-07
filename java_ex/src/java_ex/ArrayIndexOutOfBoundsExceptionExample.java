2df2ffb10b0ab4e37653ef66745832c73ce59cfe
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-07 14:01:48 +0900

    CREATE NumberFormatException

---
+++ java_ex/src/java_ex/ArrayIndexOutOfBoundsExceptionExample.java
@@ -0,0 +1,12 @@
+package java_ex;
+
+public class ArrayIndexOutOfBoundsExceptionExample {
+	
+	public static void main(String[] args) {
+		String data1 = args[0];
+		String data2 = args[1];
+		
+		System.out.println(data1);
+		System.out.println(data2);
+	}
+}
--- java_ex/src/java_ex/NullPointerExceptionExample.java
+++ java_ex/src/java_ex/NullPointerExceptionExample.java
@@ -4,6 +4,7 @@ public class NullPointerExceptionExample {
 	
 	public static void main(String[] args) {
 		String data = null;
+		data = "asdf";
 		System.out.println(data.toString());
 	}
 }
