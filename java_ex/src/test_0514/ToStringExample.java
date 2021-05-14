ff4253a505395b6bf3b86e63236f73bb2200808e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-14 14:37:34 +0900

    CREATE ToStringExample

---
+++ java_ex/src/test_0514/ToStringExample.java
@@ -0,0 +1,13 @@
+package test_0514;
+
+public class ToStringExample {
+
+	public static void main(String[] args) {
+		Object obj = new Object();
+		System.out.println(obj.toString());
+		
+		String name = new String("name");
+		System.out.println(name.toString());
+		System.out.println(name);
+	}
+}
