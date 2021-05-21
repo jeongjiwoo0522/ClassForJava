a5fcd3843fb82eb477f082cca7c213ac5fa0339c
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-21 14:19:16 +0900

    CREATE StringClassTest

---
+++ java_ex/src/test_0521/StringClassTest.java
@@ -0,0 +1,14 @@
+package test_0521;
+
+public class StringClassTest {
+
+	public static void main(String[] args) {
+		byte[] bytes = {72, 101, 108, 111, 32, 74, 97, 118, 97};
+		
+		String str1 = new String(bytes);
+		System.out.println(str1);
+		
+		String str2 = new String(bytes, 5, 4);
+		System.out.println(str2);
+	}
+}
